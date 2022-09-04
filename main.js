f31c25976a0c2e7f5567a348c74877822534c705
Anankaaa <hegyaljai83@hotmail.com>
2022-09-04 19:22:05 +0200

    done

--- main.js
+++ main.js
@@ -40,66 +40,69 @@ const cardsData = [
         img: "cards/kép5.JPG"
     }
 ];
-cardsData.sort(() => 0.5 - Math.random())
+cardsData.sort(() => 0.5 - Math.random());
 
-const timer = document.querySelector(".timer")
-const win = document.queryCommandIndeterm(".win")
+const timer = document.querySelector(".timer");
 
-let cards = []
-let chosenCards = []
-let foundPairs = []
+let cards = [];
+let chosenCards = [];
+let foundPairs = [];
 
-const resetCards = () => {
-    console.log(foundPairs)
+let firstClick = true;
+let elapsedTime = 0;
+
+const unflipCards = () => {
     cards.forEach((card, idx) => {
-        if(foundPairs.includes(idx) === false) {
-            card.classList.remove('flipped')
+        if (foundPairs.includes(idx) === false) {
+            card.classList.remove('flipped');
         }
-    })
+    });
 }
 
 const checkMatch = () => {
-    let dataId1 = parseInt(chosenCards[0].getAttribute('data-id'))
-    let dataId2 = parseInt(chosenCards[1].getAttribute('data-id'))
+    let dataId1 = parseInt(chosenCards[0].getAttribute('data-id'));
+    let dataId2 = parseInt(chosenCards[1].getAttribute('data-id'));
 
     if (cardsData[dataId1].name === cardsData[dataId2].name) {
-        console.log("Match!")
-        foundPairs.push(dataId1)
-        foundPairs.push(dataId2)
-    } else {
-        console.log("Not match!")
+        foundPairs.push(dataId1);
+        foundPairs.push(dataId2);
     }
 
     if (foundPairs.length === cardsData.length) {
-        console.log("Win!")
+        firstClick = true;
+        setTimeout(resetGame, 5000);
     } else {
-        chosenCards = []
-        resetCards()
+        chosenCards = [];
+        unflipCards();
     }
 }
 
 const clickCard = (card) => {
+    if (firstClick) {
+        firstClick = false;
+    }
     if (!chosenCards.includes(card) && !foundPairs.includes(card)) {
-        card.classList.toggle('flipped')
-        chosenCards.push(card)
+        card.classList.toggle('flipped');
+        chosenCards.push(card);
         if (chosenCards.length === 2) {
-            setTimeout(checkMatch, 500)
+            setTimeout(checkMatch, 500);
         }
     }
 }
 
 const addEventListenersToCards = () => {
-    cards = document.querySelectorAll('.card')
-    
+    cards = document.querySelectorAll('.card');
+
     cards.forEach(card => {
-        card.addEventListener('click', () => clickCard(card))
+        card.addEventListener('click', () => clickCard(card));
     })
 }
 
 const createCards = () => {
-    const boardContainer = document.querySelector(".boardContainer")
+    const boardContainer = document.querySelector(".boardContainer");
+    boardContainer.innerHTML = "";
 
-    for (let i=0; i<cardsData.length; i++) {
+    for (let i = 0; i < cardsData.length; i++) {
         let templateCard = `
         <div class="card" data-id="${i}">    
             <div class="cardFace">
@@ -117,5 +120,30 @@ const init = () => {
     createCards();
     addEventListenersToCards();
 };
+
+const resetGame = () => {
+    firstClick = true;
+    elapsedTime = 0;
+    chosenCards = [];
+    foundPairs = [];
+
+    timer.innerHTML = "00:00";
+
+    cardsData.sort(() => 0.5 - Math.random());
+    init();
+}
+
+setInterval(() => {
+    if (!firstClick) {
+        elapsedTime += 1;
+        let minutes = Math.floor(elapsedTime / 60);
+        let seconds = elapsedTime - 60 * minutes;
+        timer.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+        if (elapsedTime >= 300) {
+            resetGame();
+        }
+    }
+}, 1000);
 
 init();
